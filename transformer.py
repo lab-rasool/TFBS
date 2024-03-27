3845a23ab7969b7277832d92b811cd721a4310c5
Aakash-Tripathi <aakash.tripathi0304@gmail.com>
2024-03-27 18:30:43 -0400

    AUC calcualtion

--- transformer.py
+++ transformer.py
@@ -1,6 +1,5 @@
 # --------------------------------------------
 # TODO: Refactor
-# TODO: Metrics like ROC-AUC
 # TODO: Generate attribution maps of the CNN
 # --------------------------------------------
 
@@ -8,11 +7,15 @@ import csv
 import gzip
 
 import numpy as np
+import optuna
 import torch
 import torch.nn as nn
 import torch.optim as optim
 from rich.progress import Progress
 from rich.traceback import install
+from sklearn import metrics
+from sklearn.metrics import roc_auc_score
+from sklearn.model_selection import KFold
 from torch.utils.data import DataLoader, Dataset
 
 install()
@@ -21,64 +24,56 @@ install()
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
-# Define the CNN model
 class CNNEmbedding(nn.Module):
-    def __init__(self, embedding_dim):
+    def __init__(self, embedding_dim, dropout_rate=0.5):
         super(CNNEmbedding, self).__init__()
-        self.conv1 = nn.Conv1d(4, 64, kernel_size=3, padding=1)
+        self.conv1 = nn.Conv1d(4, 32, kernel_size=3, padding=1)
         self.relu = nn.ReLU()
         self.maxpool = nn.MaxPool1d(kernel_size=2)
-        self.conv2 = nn.Conv1d(64, 128, kernel_size=3, padding=1)
+        self.dropout = nn.Dropout(dropout_rate)
+        self.conv2 = nn.Conv1d(32, 128, kernel_size=3, padding=1)
         self.fc = nn.Linear(128 * 36, embedding_dim)
 
     def forward(self, x):
-        # print("---------------- CNN Embedding -----------------")
-        # print("1", x.shape)
         x = self.conv1(x)
-        # print("2", x.shape)
         x = self.relu(x)
-        # print("3", x.shape)
         x = self.maxpool(x)
-        # print("4", x.shape)
+        x = self.dropout(x)
         x = self.conv2(x)
-        # print("5", x.shape)
         x = self.relu(x)
-        # print("6", x.shape)
         x = self.maxpool(x)
-        # print("7", x.shape)
         x = x.view(x.size(0), -1)
-        # print("8", x.shape)
         x = self.fc(x)
-        # print("9", x.shape)
-        # print("---------------- CNN Embedding -----------------")
         return x
 
 
 # Define the transformer model
 class TransformerModel(nn.Module):
-    def __init__(self, embedding_dim):
+    def __init__(
+        self,
+        embedding_dim,
+        dropout_rate=0.5,
+        dim_feedforward=512,
+        nhead=8,
+        num_encoder_layers=6,
+        num_decoder_layers=6,
+    ):
         super(TransformerModel, self).__init__()
-        self.embedding = CNNEmbedding(embedding_dim)
+        self.embedding = CNNEmbedding(embedding_dim, dropout_rate)
         self.transformer = nn.Transformer(
-            nhead=8,
-            num_encoder_layers=6,
-            num_decoder_layers=6,
+            nhead=nhead,
+            num_encoder_layers=num_encoder_layers,
+            num_decoder_layers=num_decoder_layers,
             d_model=embedding_dim,
-            dim_feedforward=512,
+            dim_feedforward=dim_feedforward,
             batch_first=True,
         )
-        self.fc = nn.Linear(embedding_dim, 1)  # Adjusted input size
+        self.fc = nn.Linear(embedding_dim, 1)
 
     def forward(self, src):
-        # print("---------------- Transformer -----------------")
-        # print("0", src.shape)
         src = self.embedding(src)
-        # print("10", src.shape)
         output = self.transformer(src, src)
-        # print("11", output.shape)
         output = self.fc(output)
-        # print("12", output.shape)
-        # print("---------------- Transformer -----------------")
         return output
 
 
@@ -134,106 +129,247 @@ class chipseq_dataset(Dataset):
         return self.len
 
 
-chipseq = ChipDataLoader(
-    "/mnt/f/Projects/GenomicAttributions/data/encode/GATA1_K562_GATA-1_USC_AC.seq.gz"
-)
-alldataset = chipseq.load_data()
-train_dataset_pad = alldataset
-size = int(len(train_dataset_pad) / 3)
-firstvalid = train_dataset_pad[:size]
-secondvalid = train_dataset_pad[size : size + size]
-thirdvalid = train_dataset_pad[size + size :]
-firsttrain = secondvalid + thirdvalid
-secondtrain = firstvalid + thirdvalid
-thirdtrain = firstvalid + secondvalid
+def load_data(filename):
+    chipseq = ChipDataLoader(filename)
+    return chipseq.load_data()
 
-train1_dataset = chipseq_dataset(firsttrain)
-train2_dataset = chipseq_dataset(secondtrain)
-train3_dataset = chipseq_dataset(thirdtrain)
-valid1_dataset = chipseq_dataset(firstvalid)
-valid2_dataset = chipseq_dataset(secondvalid)
-valid3_dataset = chipseq_dataset(thirdvalid)
 
-batch_size = 64
-train_loader1 = DataLoader(dataset=train1_dataset, batch_size=batch_size, shuffle=True)
-train_loader2 = DataLoader(dataset=train2_dataset, batch_size=batch_size, shuffle=True)
-train_loader3 = DataLoader(dataset=train3_dataset, batch_size=batch_size, shuffle=True)
-valid1_loader = DataLoader(dataset=valid1_dataset, batch_size=batch_size, shuffle=False)
-valid2_loader = DataLoader(dataset=valid2_dataset, batch_size=batch_size, shuffle=False)
-valid3_loader = DataLoader(dataset=valid3_dataset, batch_size=batch_size, shuffle=False)
-train_dataloader = [train_loader1, train_loader2, train_loader3]
-valid_dataloader = [valid1_loader, valid2_loader, valid3_loader]
+def initialize_weights(m):
+    if isinstance(m, nn.Conv1d) or isinstance(m, nn.Linear):
+        nn.init.kaiming_uniform_(m.weight.data)
+    elif isinstance(m, nn.BatchNorm1d):
+        nn.init.constant_(m.weight.data, 1)
+        nn.init.constant_(m.bias.data, 0)
 
-embedding_dim = 768
-model = TransformerModel(embedding_dim).to(device)
 
-print(model)
+# Training function
+def train_model(
+    model,
+    train_dataloader,
+    valid_dataloader,
+    optimizer,
+    criterion,
+    num_epochs,
+    save_model=False,
+):
+    model.apply(initialize_weights)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = model.to(device)
 
-criterion = nn.MSELoss()
-# criterion = nn.BCEWithLogitsLoss()
+    best_auc = 0
+    with Progress() as progress:
+        task = progress.add_task("[cyan]Training...", total=num_epochs)
 
-optimizer = optim.Adam(model.parameters(), lr=0.0001)
-# optimizer = optim.SGD(model.parameters(), lr=0.001)
-
-# Training loop with rich progress tracking
-num_epochs = 10
-
-with Progress() as progress:
-    task = progress.add_task("[cyan]Training...", total=num_epochs)
-
-    for epoch in range(num_epochs):
-        for i, train_loader in enumerate(train_dataloader):
-            # INPUT: [batch, 4, 147]
-            # TARGET: [batch, 1]
-            # MODEL: [batch, 1]
-
-            batch_input, batch_target = next(iter(train_loader))
-            batch_input, batch_target = batch_input.to(device), batch_target.to(device)
-
-            # Forward pass
-            outputs = model(batch_input)
-            loss = criterion(outputs, batch_target)
-
-            # Backward pass and optimization
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-
-        progress.update(task, advance=1)
-        print(f"Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}")
-
-    print("Training finished!")
-
-    for i, valid_loader in enumerate(valid_dataloader):
-        with torch.no_grad():
-            for batch_input, batch_target in valid_loader:
+        for epoch in range(num_epochs):
+            model.train()
+            train_loss = 0
+            for i, train_loader in enumerate(train_dataloader):
+                batch_input, batch_target = next(iter(train_loader))
                 batch_input, batch_target = (
                     batch_input.to(device),
                     batch_target.to(device),
                 )
+                optimizer.zero_grad()
                 outputs = model(batch_input)
-                loss = criterion(outputs, batch_target)
+                loss = criterion(outputs.squeeze(), batch_target.squeeze())
+                if torch.isnan(loss):
+                    print("NaN detected in loss, stopping training")
+                    return -1  # or handle appropriately
+                loss.backward()
 
-            print(f"Validation Loss: {loss.item():.4f}")
+                # Implementing gradient clipping
+                torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
 
-    print("Validation finished!")
+                optimizer.step()
+                train_loss += loss.item()
+
+            train_loss /= len(train_dataloader)
+
+            model.eval()
+            valid_loss = 0
+            all_preds = []
+            all_targets = []
+            with torch.no_grad():
+                for i, valid_loader in enumerate(valid_dataloader):
+                    batch_input, batch_target = next(iter(valid_loader))
+                    batch_input, batch_target = (
+                        batch_input.to(device),
+                        batch_target.to(device),
+                    )
+                    outputs = model(batch_input)
+                    loss = criterion(outputs.squeeze(), batch_target.squeeze())
+                    valid_loss += loss.item()
+
+                    pred = torch.sigmoid(outputs).squeeze().cpu().numpy()
+                    all_preds.extend(pred)
+                    all_targets.extend(batch_target.squeeze().cpu().numpy())
+
+            valid_loss /= len(valid_dataloader)
+            if np.isnan(all_preds).any() or np.isnan(all_targets).any():
+                valid_auc = 0.0
+            else:
+                valid_auc = roc_auc_score(all_targets, all_preds)
+
+            progress.update(task, advance=1)
+
+            if valid_auc > best_auc:
+                best_auc = valid_auc
+
+    if save_model:
+        torch.save(model.state_dict(), "transformer.pth")
+
+    return best_auc
 
 
-# test on all data
-test_dataset = chipseq_dataset(alldataset)
-test_batch_size = test_dataset.len
-test_loader = DataLoader(
-    dataset=test_dataset, batch_size=test_batch_size, shuffle=False
-)
+def test_model(model, test_loader, device):
+    model.eval()
+    auc = []
+    with torch.no_grad():
+        for data, target in test_loader:
+            data = data.to(device)
+            target = target.to(device)
 
-with torch.no_grad():
-    losses = []
-    for batch_input, batch_target in test_loader:
-        batch_input, batch_target = batch_input.to(device), batch_target.to(device)
-        outputs = model(batch_input)
-        loss = criterion(outputs, batch_target)
-        losses.append(loss.item())
+            output = model(data)
+            pred_sig = torch.sigmoid(output)
+            pred = pred_sig.cpu().detach().numpy().reshape(output.shape[0])
+            labels = target.cpu().numpy().reshape(output.shape[0])
+            auc.append(metrics.roc_auc_score(labels, pred))
 
-    print(f"Test Loss: {np.mean(losses):.4f}")
+            print(labels, pred)
 
-torch.cuda.empty_cache()
+    print(f"Test AUC: {np.mean(auc):.4f}")
+    return np.mean(auc)
+
+
+def objective(trial):
+    # Hyperparameter settings
+    batch_size = trial.suggest_int("batch_size", 32, 128)
+    lr = trial.suggest_float("lr", 1e-5, 1e-3, log=True)
+    embedding_dim = trial.suggest_categorical(
+        "embedding_dim", [128, 256, 512, 768, 1024, 2048]
+    )
+    dropout_rate = trial.suggest_float("dropout_rate", 0.1, 0.5)
+    num_encoder_layers = trial.suggest_int("num_encoder_layers", 2, 6)
+    num_decoder_layers = trial.suggest_int("num_decoder_layers", 2, 6)
+
+    # Data loading
+    chipseq = ChipDataLoader(
+        "/mnt/f/Projects/GenomicAttributions/data/encode/GATA1_K562_GATA-1_USC_AC.seq.gz"
+    )
+    alldataset = chipseq.load_data()
+
+    # Splitting data into train and validation sets
+    kf = KFold(n_splits=3, shuffle=True, random_state=42)
+    splits = kf.split(alldataset)
+
+    train_dataloader = []
+    valid_dataloader = []
+
+    for train_index, valid_index in splits:
+        train_dataset = [alldataset[i] for i in train_index]
+        valid_dataset = [alldataset[i] for i in valid_index]
+
+        train_loader = DataLoader(
+            dataset=chipseq_dataset(train_dataset), batch_size=batch_size, shuffle=True
+        )
+        valid_loader = DataLoader(
+            dataset=chipseq_dataset(valid_dataset), batch_size=batch_size, shuffle=False
+        )
+
+        train_dataloader.append(train_loader)
+        valid_dataloader.append(valid_loader)
+
+    # Model and training
+    model = TransformerModel(
+        embedding_dim=embedding_dim,
+        dropout_rate=dropout_rate,
+        num_encoder_layers=num_encoder_layers,
+        num_decoder_layers=num_decoder_layers,
+    )
+    optimizer = optim.AdamW(model.parameters(), lr=lr)
+    criterion = nn.BCEWithLogitsLoss()
+
+    return train_model(
+        model, train_dataloader, valid_dataloader, optimizer, criterion, 100
+    )
+
+
+def main():
+    # ==============================
+    # Hyperparameter tuning
+    # ==============================
+    study = optuna.create_study(
+        direction="maximize",
+        storage="sqlite:///transformer.db",
+        study_name="transformer",
+        load_if_exists=True,
+    )
+    study.optimize(objective, n_trials=100)
+    print("Number of finished trials:", len(study.trials))
+    print("Best trial:", study.best_trial.params)
+    study = optuna.load_study(
+        study_name="transformer",
+        storage="sqlite:///transformer.db",
+    )
+
+    # ==============================
+    # Training
+    # ==============================
+    chipseq = ChipDataLoader(
+        "/mnt/f/Projects/GenomicAttributions/data/encode/GATA1_K562_GATA-1_USC_AC.seq.gz"
+    )
+    alldataset = chipseq.load_data()
+    kf = KFold(n_splits=3, shuffle=True, random_state=42)
+    splits = kf.split(alldataset)
+    train_dataloader = []
+    valid_dataloader = []
+    batch_size = study.best_trial.params["batch_size"]
+    for train_index, valid_index in splits:
+        train_dataset = [alldataset[i] for i in train_index]
+        valid_dataset = [alldataset[i] for i in valid_index]
+        train_loader = DataLoader(
+            dataset=chipseq_dataset(train_dataset), batch_size=batch_size, shuffle=True
+        )
+        valid_loader = DataLoader(
+            dataset=chipseq_dataset(valid_dataset), batch_size=batch_size, shuffle=False
+        )
+        train_dataloader.append(train_loader)
+        valid_dataloader.append(valid_loader)
+    model = TransformerModel(
+        embedding_dim=study.best_trial.params["embedding_dim"],
+        dropout_rate=study.best_trial.params["dropout_rate"],
+        num_encoder_layers=study.best_trial.params["num_encoder_layers"],
+        num_decoder_layers=study.best_trial.params["num_decoder_layers"],
+    ).to(device)
+    optimizer = optim.AdamW(model.parameters(), lr=study.best_trial.params["lr"])
+    criterion = nn.BCEWithLogitsLoss()
+    train_model(
+        model,
+        train_dataloader,
+        valid_dataloader,
+        optimizer,
+        criterion,
+        250,
+        save_model=True,
+    )
+
+    # ==============================
+    # Testing
+    # ==============================
+    # "/mnt/f/Projects/GenomicAttributions/data/encode/GATA1_K562_GATA-1_USC_AC.seq.gz"
+
+    chipseq_test = ChipDataLoader(
+        "/mnt/f/Projects/GenomicAttributions/data/encode/GATA1_K562_GATA-1_USC_B.seq.gz"
+    )
+    testdataset = chipseq_test.load_data()
+    test_dataset = chipseq_dataset(testdataset)
+    test_batch_size = test_dataset.len
+    test_loader = DataLoader(
+        dataset=test_dataset, batch_size=test_batch_size, shuffle=False
+    )
+    model.load_state_dict(torch.load("transformer.pth"))
+    test_model(model, test_loader, device)
+
+
+if __name__ == "__main__":
+    main()
